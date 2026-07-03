9385cf0f218eaf725ae7c97a654cc46696d49ac9
MJ-9527 <3285501430@qq.com>
2026-07-03 10:06:45 +0800

    1.添加了ListVersions的列出历史版本的响应结构 2.修改了GetConfig的错误响应502—>400

--- internal/handler/config.go
+++ internal/handler/config.go
@@ -18,6 +18,7 @@ func NewConfigHandler(configService *service.ConfigService) *ConfigHandler {
 	}
 }
 
+// PublishConfig 推送Config响应结构
 func (h *ConfigHandler) PublishConfig(c *gin.Context) {
 	var req model.PublishConfigRequest
 	if err := c.ShouldBindJSON(&req); err != nil {
@@ -47,13 +48,41 @@ func (h *ConfigHandler) PublishConfig(c *gin.Context) {
 
 }
 
+// GetConfig 读取 Config 响应结构
 func (h *ConfigHandler) GetConfig(c *gin.Context) {
 	app := c.Query("app")
 	env := c.Query("env")
 
 	resp, err := h.service.GetCurrent(c.Request.Context(), app, env)
 	if err != nil {
-		status := http.StatusBadGateway
+		status := http.StatusBadRequest
+		if err.Error() == "config not found" {
+			status = http.StatusNotFound
+		}
+
+		c.JSON(status, model.APIResponse{
+			Code:    40001,
+			Message: err.Error(),
+			Data:    nil,
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, model.APIResponse{
+		Code:    0,
+		Message: "success",
+		Data:    resp,
+	})
+}
+
+// ListVersions 列出历史版本
+func (h *ConfigHandler) ListVersions(c *gin.Context) {
+	app := c.Query("app")
+	env := c.Query("env")
+
+	resp, err := h.service.ListVersions(c.Request.Context(), app, env)
+	if err != nil {
+		status := http.StatusBadRequest
 		if err.Error() == "config not found" {
 			status = http.StatusNotFound
 		}
