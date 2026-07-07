77ca36111513fc925b38ddbcf6acdd292bb0625b
MJ-9527 <3285501430@qq.com>
2026-07-07 15:27:36 +0800

    规范返回信息

--- internal/handler/health.go
+++ internal/handler/health.go
@@ -1,19 +1,11 @@
 package handler
 
 import (
-	"net/http"
-
-	"GoLiteConfig/internal/model"
-
 	"github.com/gin-gonic/gin"
 )
 
 func Health(c *gin.Context) {
-	c.JSON(http.StatusOK, model.APIResponse{
-		Code:    0,
-		Message: "success",
-		Data: gin.H{
-			"status": "ok",
-		},
+	respondSuccess(c, gin.H{
+		"status": "ok",
 	})
 }
