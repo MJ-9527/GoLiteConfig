334400b20dce9b71b5169d9dc5bcd4885b7bf5d2
MJ-9527 <3285501430@qq.com>
2026-07-08 11:24:57 +0800

    连接HTML网页，对比配置路由接口

--- internal/router/router.go
+++ internal/router/router.go
@@ -11,14 +11,18 @@ import (
 func SetupRouter(configHandler *handler.ConfigHandler, logger *zap.Logger) *gin.Engine {
 	r := gin.New()
 	r.Use(gin.Recovery())
+	r.Use(cors())
 	r.Use(logging.RequestLogger(logger))
 
+	r.StaticFile("/", "./web/index.html")
+	r.StaticFile("/console", "./web/index.html")
 	r.GET("/health", handler.Health)
 
 	api := r.Group("/api")
 	{
 		api.POST("/config", configHandler.PublishConfig)
 		api.GET("/config", configHandler.GetConfig)
+		api.GET("/config/diff", configHandler.DiffVersions)
 		api.GET("/config/versions", configHandler.ListVersions)
 		api.POST("/config/versions/delete", configHandler.DeleteVersions)
 		api.POST("/config/rollback", configHandler.Rollback)
